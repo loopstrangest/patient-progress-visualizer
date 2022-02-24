ab240e10ecdac7d2f5fab5a8646b77f31648a5ce
Dalton Dinderman <loopstrangest@gmail.com>
2022-02-24 14:59:05 -0600

    Initial commit: library install, file/folder setup

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box
+}
+
+h1{
+    font-size: 24px;
+}
+
+`;
+
+export default GlobalStyle;
