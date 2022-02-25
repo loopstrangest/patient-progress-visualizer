a523f94c90d7bc8f4eb830dec06812d8b529e478
Dalton Dinderman <loopstrangest@gmail.com>
2022-02-24 19:54:15 -0600

    LineChart component, sample patient data display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, {useState,useEffect} from 'react';
 import GlobalStyle from "./components/GlobalStyle";
+import patientData from "./MH Score Visualizer Data.json"
+import LineChartDisplay from "./components/LineChart";
+
+//patientData['newField'] = patientData['Timestamp'].getTime();
+patientData.forEach(entry => {
+  var d = new Date(entry['Timestamp']);
+  entry["Date"] = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear();
+  entry["Unixtime"] = d.getTime();
+});
+
+var defaultPatientData = patientData.filter(data => data["Patient Name"] == "Lianna");
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <h1>Patient Progress Visualizer</h1>
+      <h2>A simple visual summary of your patients' progress</h2>
+      <div className="LineChartContainer" style={{width: '100%', height: '500px'}}>
+        <LineChartDisplay data={defaultPatientData}/>
+      </div>
+      
+
     </div>
   );
 }
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -5,11 +5,43 @@ const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    font-family: -apple-system,BlinkMacSystemFont,Inter,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
 }
 
-h1{
+html{
+    font-family: sans-serif;
+}
+
+.App{
+    display: flex;
+    flex-direction: column;
+}
+
+.LineChartContainer{
+    display:flex;
+    flex-direction: row;
+    margin: auto;
+}
+
+.recharts-responsive-container{
+    margin: auto;
+}
+
+h1, h2{
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
     font-size: 24px;
+    color: #278c79;
+    margin:auto;
+    margin-top: 0.5rem;
+    text-align: center;
+}
+
+h2{
+    font-size: 18px;
+    line-height: 18px;
+    margin-bottom: 1rem;
 }
 
 `;
